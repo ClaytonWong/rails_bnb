f2dc245bf6ded1476b3e5399c0a4f0a72a9478e6
Clayton <c_w_serious-github@yahoo.com.au>
2017-10-12 11:35:04 +1100

    Add validations and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Takes 10000 and output it as $100.00
+  def format_cents(cents)
+    money = Money.new(cents, 'AUD')
+    money.format
+  end
+
+  def format_cents_for_country(cents, country)
+    money = Money.new(cents, country.currency.iso_code)
+    money.format
+  end
 end
